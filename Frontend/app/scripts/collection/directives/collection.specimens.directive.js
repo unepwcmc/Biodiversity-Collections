fe3d791a3c01668acf86fc14a83ce91d7d45f537
integritas-jozecarlos <i4525001314s>
2015-11-19 12:16:19 -0200

    specimens pagination

--- Frontend/app/scripts/collection/directives/collection.specimens.directive.js
+++ Frontend/app/scripts/collection/directives/collection.specimens.directive.js
@@ -3,24 +3,46 @@
  * @author: jozecarlos.it@gmail.com
  *
  */
-define(['angularAMD'], function (angularAMD) {
+define(['angularAMD','core/factory/specimenFactory'], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('specimens', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies',
+    angularAMD.directive('specimens', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies','Specimen',
 
-        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies) {
+        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies, Specimen) {
 
             return {
                 restrict: 'E',
                 templateUrl: 'views/collection/specimens.tpl.html',
-
+                scope:{
+                    collection: '='
+                },
                 controller: ['$scope', '$rootScope', '$stateParams', '$translate',
                     function($scope, $rootScope, $stateParams, $translate){
-                        //Empty
+
+                        $scope.page  = {totalElements : 0, number: 0, size: 10, totalPages: 0};
+                        $scope.specimens = new Specimen();
+                        $scope.specimens.load($scope.collection,  $scope.page.number, $scope.page.size);
+
+                        $scope.$on('SPECIMENS_LOADED', function( ) {
+                            console.log('Specimens Loaded...');
+
+                            $scope.page.number = $scope.specimens.number;
+                            $scope.page.size = $scope.specimens.size;
+                            $scope.page.totalPages = $scope.specimens.totalPages;
+                            $scope.page.totalElements = $scope.specimens.totalElements;
+                        });
+
+                        $scope.paginate = function(page, size){
+                            $scope.specimens.load($scope.collection , page, size);
+                        };
                     }],
                 link: function (scope, element, attrs) {
-                    // Empty
+
+                    $("#specimen-size-box").change(function() {
+                        scope.page.size = parseInt($(this).val());
+                        scope.paginate(scope.number, $(this).val())
+                    });
                 }
             };
         }]);
