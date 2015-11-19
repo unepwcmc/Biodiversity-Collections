3e7aef14e2e399bbdc8f58e2caedd5226436a597
integritas-jozecarlos <i4525001314s>
2015-11-19 12:35:20 -0200

    publication pagination

--- Frontend/app/scripts/collection/directives/collection.publications.directive.js
+++ Frontend/app/scripts/collection/directives/collection.publications.directive.js
@@ -3,24 +3,47 @@
  * @author: jozecarlos.it@gmail.com
  *
  */
-define(['angularAMD'], function (angularAMD) {
+define(['angularAMD','core/factory/documentFactory'], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('publications', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies',
+    angularAMD.directive('publications', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies','Document',
 
-        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies) {
+        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies, Document) {
 
             return {
                 restrict: 'E',
                 templateUrl: 'views/collection/publications.tpl.html',
-
+                scope:{
+                    collection: '='
+                },
                 controller: ['$scope', '$rootScope', '$stateParams', '$translate',
                     function($scope, $rootScope, $stateParams, $translate){
-                        //Empty
+
+                        $scope.page  = {totalElements : 0, number: 0, size: 10, totalPages: 0};
+                        $scope.documents = new Document();
+                        $scope.documents.load($scope.collection, $scope.page.number, $scope.page.size);
+
+                        $scope.$on('NETWORK_LOADED', function( ) {
+                            console.log('Networks Loaded...');
+
+                            $scope.page.number = $scope.documents.number;
+                            $scope.page.size = $scope.documents.size;
+                            $scope.page.totalPages = $scope.documents.totalPages;
+                            $scope.page.totalElements = $scope.documents.totalElements;
+                        });
+
+                        $scope.paginate = function(page, size){
+                            $scope.documents.load($scope.collection , page, size);
+                        };
+
                     }],
                 link: function (scope, element, attrs) {
-                    // Empty
+
+                    $("#network-size-box").change(function() {
+                        scope.page.size = parseInt($(this).val());
+                        scope.paginate(scope.number, $(this).val())
+                    });
                 }
             };
         }]);
