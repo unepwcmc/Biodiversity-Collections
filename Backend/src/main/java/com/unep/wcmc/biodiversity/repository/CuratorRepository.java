c393b91c037cec664a0ea220284071f21292e52d
integritas-jozecarlos <i4525001314s>
2015-11-19 12:02:53 -0200

    Adding methods on curator repository

--- Backend/src/main/java/com/unep/wcmc/biodiversity/repository/CuratorRepository.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/repository/CuratorRepository.java
@@ -1,8 +1,17 @@
 package com.unep.wcmc.biodiversity.repository;
 
 import com.unep.wcmc.biodiversity.model.Curator;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
+@RepositoryRestResource(path = "curators")
 public interface CuratorRepository extends JpaRepository<Curator, Long> {
+
+    @RestResource(path = "name", rel = "name")
+    public Page findByNameStartsWith(@Param("name") String name, Pageable p);
 }
