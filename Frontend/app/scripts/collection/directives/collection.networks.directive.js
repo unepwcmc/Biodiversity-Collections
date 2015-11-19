83fbe37f6b22e260b8f4991c751aceb6ecbe7637
integritas-jozecarlos <i4525001314s>
2015-11-19 12:15:29 -0200

    network pagination

--- Frontend/app/scripts/collection/directives/collection.networks.directive.js
+++ Frontend/app/scripts/collection/directives/collection.networks.directive.js
@@ -3,24 +3,47 @@
  * @author: jozecarlos.it@gmail.com
  *
  */
-define(['angularAMD'], function (angularAMD) {
+define(['angularAMD','core/factory/networkFactory'], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('networks', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies',
+    angularAMD.directive('networks', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies','Network',
 
-        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies) {
+        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies, Network) {
 
             return {
                 restrict: 'E',
                 templateUrl: 'views/collection/networks.tpl.html',
-
+                scope:{
+                    collection: '='
+                },
                 controller: ['$scope', '$rootScope', '$stateParams', '$translate',
                     function($scope, $rootScope, $stateParams, $translate){
-                        //Empty
+
+                        $scope.page  = {totalElements : 0, number: 0, size: 10, totalPages: 0};
+                        $scope.networks = new Network();
+                        $scope.networks.load($scope.collection,  $scope.page.number, $scope.page.size);
+
+                        $scope.$on('NETWORK_LOADED', function( ) {
+                            console.log('Networks Loaded...');
+
+                            $scope.page.number = $scope.networks.number;
+                            $scope.page.size = $scope.networks.size;
+                            $scope.page.totalPages = $scope.networks.totalPages;
+                            $scope.page.totalElements = $scope.networks.totalElements;
+                        });
+
+                        $scope.paginate = function(page, size){
+                            $scope.networks.load($scope.collection , page, size);
+                        };
+
                     }],
                 link: function (scope, element, attrs) {
-                    // Empty
+
+                    $("#network-size-box").change(function() {
+                        scope.page.size = parseInt($(this).val());
+                        scope.paginate(scope.number, $(this).val())
+                    });
                 }
             };
     }]);
