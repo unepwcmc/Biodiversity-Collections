4f4e5c6f7ada9dd30f9fff8422952228ee8030ca
integritas-jozecarlos <i4525001314s>
2015-11-19 12:15:59 -0200

    sample pagination

--- Frontend/app/scripts/collection/directives/collection.samples.directive.js
+++ Frontend/app/scripts/collection/directives/collection.samples.directive.js
@@ -3,24 +3,48 @@
  * @author: jozecarlos.it@gmail.com
  *
  */
-define(['angularAMD'], function (angularAMD) {
+define(['angularAMD','core/factory/sampleFactory'], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('samples', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies',
+    angularAMD.directive('samples', ['$timeout', '$rootScope', '$stateParams', '$window', '$http', '$cookies','Sample',
 
-        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies) {
+        function ($timeout, $rootScope, $stateParams, $window, $http, $cookies, Sample) {
 
             return {
                 restrict: 'E',
                 templateUrl: 'views/collection/samples.tpl.html',
-
+                scope:{
+                    collection: '='
+                },
                 controller: ['$scope', '$rootScope', '$stateParams', '$translate',
                     function($scope, $rootScope, $stateParams, $translate){
-                       //Empty
+
+                        $scope.page  = {totalElements : 0, number: 0, size: 10, totalPages: 0};
+                        $scope.samples = new Sample();
+                        $scope.samples.load($scope.collection,  $scope.page.number, $scope.page.size);
+
+                        $scope.$on('SAMPLE_LOADED', function( ) {
+                            console.log('Samples Loaded...');
+
+                            $scope.page.number = $scope.samples.number;
+                            $scope.page.size = $scope.samples.size;
+                            $scope.page.totalPages = $scope.samples.totalPages;
+                            $scope.page.totalElements = $scope.samples.totalElements;
+                        });
+
+                        $scope.paginate = function(page, size){
+                            $scope.samples.load($scope.collection , page, size);
+                        };
+
+
                     }],
                 link: function (scope, element, attrs) {
-                    // Empty
+
+                    $("#sample-size-box").change(function() {
+                        scope.page.size = parseInt($(this).val());
+                        scope.paginate(scope.number, $(this).val())
+                    });
                 }
             };
         }]);
