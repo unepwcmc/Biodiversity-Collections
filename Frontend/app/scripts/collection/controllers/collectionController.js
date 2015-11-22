8f500176fdb1425d26a447770faf0b6b31f4eb00
integritas-jozecarlos <i4525001314s>
2015-11-22 00:49:44 -0200

    collection controller saving objects

--- Frontend/app/scripts/collection/controllers/collectionController.js
+++ Frontend/app/scripts/collection/controllers/collectionController.js
@@ -2,22 +2,35 @@ define(['app','collection/directives/collection.networks.directive',
               'collection/directives/collection.details.directive',
               'collection/directives/collection.publications.directive',
               'collection/directives/collection.samples.directive',
-              'collection/directives/collection.specimens.directive'], function () {
+              'collection/directives/collection.specimens.directive',
+              'collection/directives/collection.occurrence.directive',
+              'core/factory/biodiversityCollectionFactory'], function () {
 
     'use strict';
 
-    return ['$scope','BaseController','$stateParams','$http','$rootScope',
+    return ['$scope','BaseController','$stateParams','$http','$rootScope','BiodiversityCollection',
 
-           function ($scope, BaseController, $stateParams, $http, $rootScope) {
+           function ($scope, BaseController, $stateParams, $http, $rootScope,BiodiversityCollection) {
                 angular.extend($scope, BaseController);
 
+               $scope.collection = new BiodiversityCollection();
                /**
                 * Listener when the view
                 */
                $scope.$on('$viewContentLoaded', function() {
                    console.log('view Content Loaded...');
 
-                   $scope.id = $stateParams.id;
+                   $scope.collection.get( $stateParams.id );
+               });
+
+               $scope.$on('BIODIVERSITY_COLLECTION_SAVE', function(){
+                   console.log('collection updating..');
+
+                   $scope.collection.update();
+               });
+
+               $scope.$on('BIODIVERSITY_UPDATED', function(){
+                   console.log('updated');
                });
            }
     ];
