4332f1b9f8d76d4cfcdd940d9c7d5cb60c26f2c8
integritas-jozecarlos <i4525001314s>
2015-11-22 00:53:31 -0200

    adding dependencies

--- Frontend/app/scripts/include.js
+++ Frontend/app/scripts/include.js
@@ -2,7 +2,7 @@ define(
     [
         'angularAMD','angular','underscore','angular-translate',
         'angular-ui-router','angular-toastr','angular-cookies','angucomplete-alt',
-        'angular-resource','angular-sanitize','popover','nemLogging',
+        'angular-resource','angular-sanitize','popover','nemLogging','waypoints',
         'angular-toastr-tpl','leaflet-directive', 'leaflet', 'leaflet.markercluster',
         'core/directives/core.include.template.directive',
         'core/directives/core.load.screen.directive',
@@ -13,6 +13,7 @@ define(
         'core/directives/core.main.directive',
         'core/directives/core.breadcrumbs.directive',
         'core/directives/core.edit.form.directive',
+        'core/directives/core.on.finish.render.directive',
         'core/filters/core.pagination.filter',
         'core/controllers/BaseController',
         'auth/directives/auth.username.box.directive'
