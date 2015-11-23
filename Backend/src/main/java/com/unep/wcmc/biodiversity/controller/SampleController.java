f8a1113721ec7c724e00e436ab06b81b90d76acd
Rodrigo Silva <rcandidosilva@gmail.com>
2015-11-23 13:02:28 -0200

    Changes on the @Controllers

--- Backend/src/main/java/com/unep/wcmc/biodiversity/controller/SampleController.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/controller/SampleController.java
@@ -3,6 +3,7 @@ package com.unep.wcmc.biodiversity.controller;
 import com.unep.wcmc.biodiversity.model.Sample;
 import com.unep.wcmc.biodiversity.service.BiodiversityCollectionService;
 import com.unep.wcmc.biodiversity.service.SampleService;
+import com.unep.wcmc.biodiversity.support.AbstractController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,17 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/samples")
-public class SampleController {
+public class SampleController extends AbstractController<Sample, SampleService> {
 
     @Autowired
     private BiodiversityCollectionService biodiversityCollectionService;
 
-    @Autowired
-    private SampleService service;
-
     @RequestMapping(method = RequestMethod.GET, value = "/search/collection/{collectionId}")
     public Page<Sample> findAllSampleByCollection(@PathVariable Long collectionId, @PageableDefault(page = 0, size = 10) Pageable pageable) {
-
         return service.findAllByCollection(biodiversityCollectionService.get(collectionId), pageable);
     }
 }
