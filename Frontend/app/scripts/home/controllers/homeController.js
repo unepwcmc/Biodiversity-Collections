cf062668325e0a29169eaeccb83d2baac7a3e2a0
integritas-fernandoerico <fernandoericofilho@integritas.com.br>
2015-11-24 21:04:09 -0200

    Adjust search filter in home page

--- Frontend/app/scripts/home/controllers/homeController.js
+++ Frontend/app/scripts/home/controllers/homeController.js
@@ -37,11 +37,9 @@ define(['app', 'core/directives/core.map.directive','core/factory/biodiversityCo
                 $state.go('search', { term : $scope.term });
             };
 
-            $scope.autocomplete = function( input ){
-                $scope.collections.autocomplete( input, function( data){
-                      console.log(data._embedded.biodiversityCollections);
-                });
-            };
+            $scope.isSearchEnabled = function() {
+                return !($scope.term != "" && $scope.term.length >= 3 && $scope.search.type != undefined && && $scope.search.type != "");
+            }
 
         }];
 });
