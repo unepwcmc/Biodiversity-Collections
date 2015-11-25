075176947e82184b07fa072ff6c1cd4ffb148417
integritas-jozecarlos <i4525001314s>
2015-11-25 12:58:25 -0200

    publications pagination

--- Frontend/app/scripts/collection/directives/collection.publications.directive.js
+++ Frontend/app/scripts/collection/directives/collection.publications.directive.js
@@ -17,29 +17,24 @@ define(['angularAMD','core/factory/documentFactory','core/directives/core.table.
                 controller: ['$scope', '$rootScope', '$stateParams', '$translate',
                     function($scope, $rootScope, $stateParams, $translate){
 
-                        $scope.page  = {totalElements : 0, number: 0, size: 10, totalPages: 0};
                         $scope.documents = new Document();
-                        $scope.documents.load( $stateParams.id, $scope.page.number, $scope.page.size);
+                        angular.extend($scope.documents, {totalElements : 0, number: 0, size: 5, totalPages: 0});
+                        $scope.documents.load($stateParams.id, $scope.documents.number, $scope.documents.size);
 
                         $scope.$on('NETWORK_LOADED', function( ) {
                             console.log('Publications Loaded...');
-
-                            $scope.page.number = $scope.documents.number;
-                            $scope.page.size = $scope.documents.size;
-                            $scope.page.totalPages = $scope.documents.totalPages;
-                            $scope.page.totalElements = $scope.documents.totalElements;
                         });
 
-                        $scope.paginate = function(page, size){
+                        $scope.paginatePublications = function(page, size){
                             $scope.documents.load( $stateParams.id , page, size);
                         };
 
                     }],
                 link: function (scope, element, attrs) {
 
-                    $("#network-size-box").change(function() {
-                        scope.page.size = parseInt($(this).val());
-                        scope.paginate(scope.number, $(this).val())
+                    $("#publication-size-box").change(function() {
+                        scope.documents.size = parseInt($(this).val());
+                        scope.paginatePublications(scope.documents.number, parseInt($(this).val()))
                     });
                 }
             };
