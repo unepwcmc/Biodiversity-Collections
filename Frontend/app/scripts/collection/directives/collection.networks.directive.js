05676efc96f5592cf892711762a72a09e6f54a99
integritas-jozecarlos <i4525001314s>
2015-11-25 12:56:58 -0200

    networks pagination

--- Frontend/app/scripts/collection/directives/collection.networks.directive.js
+++ Frontend/app/scripts/collection/directives/collection.networks.directive.js
@@ -17,20 +17,15 @@ define(['angularAMD','core/factory/networkFactory'], function (angularAMD) {
                 controller: ['$scope', '$rootScope', '$stateParams', '$translate',
                     function($scope, $rootScope, $stateParams, $translate){
 
-                        $scope.page  = {totalElements : 0, number: 0, size: 10, totalPages: 0};
                         $scope.networks = new Network();
-                        $scope.networks.load( $stateParams.id,  $scope.page.number, $scope.page.size);
+                        angular.extend($scope.networks, {totalElements : 0, number: 0, size: 5, totalPages: 0});
+                        $scope.networks.load( $stateParams.id,  $scope.networks.number, $scope.networks.size);
 
                         $scope.$on('NETWORK_LOADED', function( ) {
                             console.log('Networks Loaded...');
-
-                            $scope.page.number = $scope.networks.number;
-                            $scope.page.size = $scope.networks.size;
-                            $scope.page.totalPages = $scope.networks.totalPages;
-                            $scope.page.totalElements = $scope.networks.totalElements;
                         });
 
-                        $scope.paginate = function(page, size){
+                        $scope.paginateNetworks = function(page, size){
                             $scope.networks.load( $stateParams.id , page, size);
                         };
 
@@ -38,8 +33,8 @@ define(['angularAMD','core/factory/networkFactory'], function (angularAMD) {
                 link: function (scope, element, attrs) {
 
                     $("#network-size-box").change(function() {
-                        scope.page.size = parseInt($(this).val());
-                        scope.paginate(scope.number, $(this).val())
+                        scope.networks.size = parseInt($(this).val());
+                        scope.paginateNetworks(scope.networks.number, $(this).val())
                     });
                 }
             };
