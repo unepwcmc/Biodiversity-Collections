6285cfd810660b0aa22465bb7f9e4cf2c8019b9e
integritas-fernandoerico <fernandoericofilho@integritas.com.br>
2015-11-30 22:12:23 -0200

    Start Instituition page ID 9709

--- Frontend/app/scripts/core/factory/institutionFactory.js
+++ Frontend/app/scripts/core/factory/institutionFactory.js
@@ -68,8 +68,41 @@ define(['angularAMD'], function (angularAMD) {
                         $log.error(message);
                         $rootScope.$broadcast("INSTITUTION_LOAD_ERROR");
                     });
-            }
+            },
+            search: function( query, page, size ) {
 
+                var self = this;
+
+                $http.get( $rootScope.getHost() + "institutions/search/name?name=" + query + "&page=" + page + "&size=" + size )
+
+                    .success(function (data) {
+                        self.setData(data);
+                        $rootScope.$broadcast("INSTITUTION_SEARCHED", data);
+                    })
+                    .error(function (message) {
+                        $log.error(message);
+                        $rootScope.$broadcast("INSTITUTION_SEARCHED_SEARCH_ERROR");
+                    });
+            },
+            autocompleteName: function( query, callback ) {
+
+                return $http.get( $rootScope.getHost() + "institutions/search/autocompleteName?name=" + query  )
+                    .success(function (data) {
+                        if (data.message == 'no matches found') {
+                            $rootScope.$broadcast("INSTITUTION_AUTOCOMPLETE_LOAD_ERROR");
+                        } else {
+                            $rootScope.$broadcast("INSTITUTION_AUTOCOMPLETE_LOADED", data);
+                            if (callback) {
+                                data._embedded.biodiversityCollections = data._embedded.institutions;
+                                callback(data);
+                            }
+                        }
+                    })
+                    .error(function (message) {
+                        $log.error(message);
+                        $rootScope.$broadcast("INSTITUTION_AUTOCOMPLETE_LOAD_ERROR");
+                    });
+            }
         };
 
         return Institution;
