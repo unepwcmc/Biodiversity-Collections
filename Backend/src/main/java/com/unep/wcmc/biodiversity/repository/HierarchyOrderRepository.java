d6b1d038e44204a8c7b60a2e8ccd83ed91c6e50c
Rodrigo Silva <rcandidosilva@gmail.com>
2015-12-03 17:22:24 -0200

    Changes on the Taxonomy model

--- Backend/src/main/java/com/unep/wcmc/biodiversity/repository/HierarchyOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unep.wcmc.biodiversity.repository;
-
-import com.unep.wcmc.biodiversity.model.HierarchyOrder;
-import com.unep.wcmc.biodiversity.support.AbstractRepository;
-
-public interface HierarchyOrderRepository extends AbstractRepository<HierarchyOrder> {
-}
--- Backend/src/main/java/com/unep/wcmc/biodiversity/repository/PhylumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unep.wcmc.biodiversity.repository;
-
-import com.unep.wcmc.biodiversity.model.Phylum;
-import com.unep.wcmc.biodiversity.support.AbstractRepository;
-
-public interface PhylumRepository extends AbstractRepository<Phylum> {
-}
