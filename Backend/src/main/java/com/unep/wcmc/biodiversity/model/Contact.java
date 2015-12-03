61629afea9d17e419db0e8d634798059e3fec421
Rodrigo Silva <rcandidosilva@gmail.com>
2015-12-02 23:13:55 -0200

    Changes on the Contact address type

--- Backend/src/main/java/com/unep/wcmc/biodiversity/model/Contact.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/model/Contact.java
@@ -8,7 +8,11 @@ public class Contact implements Serializable {
 
     private String phoneNumber;
 
-    private String address;
+    private String address1;
+
+    private String address2;
+
+    private String address3;
 
     private String city;
 
@@ -26,12 +30,28 @@ public class Contact implements Serializable {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getAddress() {
-        return address;
+    public String getAddress1() {
+        return address1;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getAddress3() {
+        return address3;
+    }
+
+    public void setAddress3(String address3) {
+        this.address3 = address3;
     }
 
     public String getCity() {
