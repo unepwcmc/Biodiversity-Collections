f2e7ffe885d0757c0c841255e0d65b6227e1ce81
integritas-jozecarlos <i4525001314s>
2015-12-20 00:05:56 -0200

    adding more fields on member model

--- Backend/src/main/java/com/unep/wcmc/biodiversity/model/Member.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/model/Member.java
@@ -1,10 +1,14 @@
 package com.unep.wcmc.biodiversity.model;
 
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Embeddable
 public class Member implements Serializable {
+
+    /*@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;*/
 
     private String name;
 
@@ -12,28 +16,24 @@ public class Member implements Serializable {
 
     private String doingWithTheCollection;
 
-    public String getName() {
-        return name;
+    @OneToOne(orphanRemoval = true)
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    /*public Long getId() {
+        return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }*/
 
     public void setName(String name) {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Member)) return false;
-
-        Member that = (Member) o;
-
-        return !(name != null ? !name.equals(that.name) : that.name != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+    public String getName() {
+        return name;
     }
 
     public String getRelationshipToTheCollection() {
@@ -51,4 +51,31 @@ public class Member implements Serializable {
     public void setDoingWithTheCollection(String doingWithTheCollection) {
         this.doingWithTheCollection = doingWithTheCollection;
     }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Member)) return false;
+
+        Member that = (Member) o;
+
+        return !(name != null ? !name.equals(that.name) : that.name != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+
 }
