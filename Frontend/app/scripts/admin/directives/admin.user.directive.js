11395c50ba12170f84b7843212b849b56fe35276
integritas-jozecarlos <i4525001314s>
2016-01-02 15:55:26 -0200

    delete all working

--- Frontend/app/scripts/admin/directives/admin.user.directive.js
+++ Frontend/app/scripts/admin/directives/admin.user.directive.js
@@ -7,7 +7,7 @@ define(['angularAMD','core/factory/userFactory'], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('adminUser', ['$timeout', '$rootScope', '$stateParams', '$state', '$window', '$http', '$cookies','toastr','User', function ($timeout, $rootScope, $stateParams, $state, $window, $http, $cookies, toastr, User) {
+    angularAMD.directive('adminUser', ['$timeout', '$rootScope', '$stateParams', '$state', '$window', '$http', '$cookies','toastr','User','$q', function ($timeout, $rootScope, $stateParams, $state, $window, $http, $cookies, toastr, User, $q) {
 
             return {
                 restrict: 'EA',
@@ -15,6 +15,8 @@ define(['angularAMD','core/factory/userFactory'], function (angularAMD) {
                 controller: ['$scope', '$rootScope', '$stateParams', '$state', '$translate',  function($scope, $rootScope, $stateParams, $state, $translate){
 
                         $scope.users = null;
+                        $scope.checkboxes_selected = false;
+                        $scope.checkboxCount = 0;
 
                         $scope.$on('ADMIN_USERS_TAB', function(){
                              console.log('users tab');
@@ -54,7 +56,66 @@ define(['angularAMD','core/factory/userFactory'], function (angularAMD) {
                                        $('#loader-wrapper').fadeToggle('400');
                                    }
                              });
-                        }
+                        };
+
+                        $scope.singleCheckBoxEvent = function($event){
+
+                            if($($event.currentTarget).is(":checked"))
+                                $scope.checkboxCount+=1;
+                            else
+                                $scope.checkboxCount-=1;
+                        };
+
+                        $scope.checkAndUnCheckAll = function(){
+
+                            $scope.checkboxes_selected = !$scope.checkboxes_selected;
+
+                            if($scope.checkboxes_selected && $scope.checkboxCount > 0){
+                                $scope.checkboxCount = 0;
+                            }
+
+                            $("input[type=checkbox].user-checkbox-delete").each(function () {
+
+                                $(this).prop("checked", $scope.checkboxes_selected);
+
+                                if($scope.checkboxes_selected)
+                                    $scope.checkboxCount +=1;
+                                else
+                                    $scope.checkboxCount -= 1;
+
+                            });
+                        };
+
+                        $scope.deleteAll = function(){
+
+                            /* if($scope.samples.number > 0){
+                             if( (($scope.samples.totalElements - $scope.samples.size) % $scope.samples.size) == 0){
+                             $scope.samples.number = $scope.samples.number - 1;
+                             $scope.samples.totalPages = $scope.samples.totalPages - 1;
+                             }
+                             }*/
+
+                            $('#loader-wrapper').fadeToggle('400');
+
+                            $('#checkbox-all').prop("checked", false);
+
+                            var promises = [];
+
+                            $("input[type=checkbox].user-checkbox-delete").each(function () {
+                                if($(this).is(":checked")){
+                                    if($(this).data('user-id') != undefined){
+                                        promises.push( $http({ method:'DELETE', url: $rootScope.getHost() + "users/" + $(this).data('user-id') }) );
+                                    }
+                                }
+                            });
+
+                            $q.all( promises ).then(function( results ){
+                                $scope.showSuccessMessage('USERS_DELETED_SUCCESSFULLY','SUCCESS');
+                                $scope.users.list($scope.users.number, $scope.users.size);
+                                $scope.checkboxes_selected = !$scope.checkboxes_selected;
+                                $scope.checkboxCount = 0;
+                            });
+                        };
 
                     }],
                 link: function (scope, element, attrs) {
