f34979281fc2231ecdad52cc258f6f75b1c1f262
integritas-jozecarlos <i4525001314s>
2016-01-15 15:40:30 -0200

    samples thumbnail.

--- Frontend/app/scripts/core/directives/core.thumbnail.directive.js
+++ Frontend/app/scripts/core/directives/core.thumbnail.directive.js
@@ -2,8 +2,7 @@ define(['angularAMD', 'core/factory/imageFactory' ], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('imageThumbnail', ['$timeout', '$rootScope', 'toastr', 'Image',
-        function ( $timeout, $rootScope, toastr, Image ) {
+    angularAMD.directive('imageThumbnail', ['$timeout','$http',   function ( $timeout, $http ) {
 
             return {
 
@@ -14,17 +13,31 @@ define(['angularAMD', 'core/factory/imageFactory' ], function (angularAMD) {
                     width:'=',
                     height:'='
                 },
-                controller: ['$scope', '$http', '$rootScope', '$state', '$q', '$stateParams', '$translate',
-                    function( $scope, $http, $rootScope, $state, $q, $stateParams, $translate ) {
+                controller: ['$scope', '$http', '$rootScope', function( $scope, $http, $rootScope) {
 
-                        $scope.root = $rootScope.getHost();
+                        $scope.loadImage = function( img ){
+
+                            if( $scope.attachment != undefined){
+
+                                img.attr('src',"/images/icons/ajax-loader.gif");
+
+                                $timeout( function(){
+
+                                    $http.get($rootScope.getHost() + "medias/" +  $scope.attachment + "/image")
+                                        .success( function( data, status){
+                                            img.attr('src',"data:image/*;base64," + data);
+                                        });
+
+                                },500);
+                            }
+                        }
 
                     }],
                 link: function (scope, element, attrs) {
 
-                    if( scope.attachment != undefined){
-                        $(element).find('img').attr('src', scope.root + "medias/" + scope.attachment+ "/image");
-                    }
+                    scope.$watch('attachment', function(newValue, oldValue){
+                        scope.loadImage($(element).find('img'));
+                    },true);
                 }
             };
         }]);
