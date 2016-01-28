40d5f46aaf20adea4bee7ec4c79f6607fe4624ce
Rodrigo Silva <rcandidosilva@gmail.com>
2016-01-28 18:11:06 -0200

    Enabled tomcat deployment configuration

--- Backend/src/main/java/com/unep/wcmc/biodiversity/controller/InstitutionController.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/controller/InstitutionController.java
@@ -36,7 +36,7 @@ public class InstitutionController extends AbstractController<Institution, Insti
 
     @Override
     public Object read(@PathVariable String id) {
-        return service.getRepository().getById(new Long(id));
+        return service.get(new Long(id));
     }
 
     @RequestMapping(method= RequestMethod.GET, value="/search/name")
--- Backend/src/main/java/com/unep/wcmc/biodiversity/service/InstitutionService.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/service/InstitutionService.java
@@ -2,6 +2,7 @@ package com.unep.wcmc.biodiversity.service;
 
 import com.unep.wcmc.biodiversity.model.Curator;
 import com.unep.wcmc.biodiversity.model.Institution;
+import com.unep.wcmc.biodiversity.model.User;
 import com.unep.wcmc.biodiversity.repository.InstitutionRepository;
 import com.unep.wcmc.biodiversity.support.AbstractService;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,18 @@ import java.util.Set;
 
 @Service
 public class InstitutionService extends AbstractService<Institution, InstitutionRepository> {
+
+    @Override
+    public Institution get(Long id) {
+        Institution result = repo.getById(id);
+        if (result != null) {
+            for (Curator curator : result.getCurators()) {
+                curator.getUser().setInstitution(null);
+            }
+        }
+        return result;
+    }
+
 
     @Override
     public Institution save(Institution institution) {
