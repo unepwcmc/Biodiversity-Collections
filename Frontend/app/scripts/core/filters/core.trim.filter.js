c7030574fcdad2bbd1ed22b5e75c252607bcfca8
Rodrigo Silva <rcandidosilva@gmail.com>
2016-02-01 23:26:58 -0200

    Trim fixes at search result's page

---
+++ Frontend/app/scripts/core/filters/core.trim.filter.js
@@ -0,0 +1,17 @@
+/**
+ * Trim Spaces Filter
+ *
+ */
+define(['angularAMD'], function (angularAMD) {
+
+    'use strict';
+
+    angularAMD.filter('trim', function () {
+        return function(value) {
+            if(!angular.isString(value)) {
+                return value;
+            }
+            return value.replace(/^\s+|\s+$/g, ''); // you could use .trim, but it's not going to work in IE<9
+        };
+    });
+});
