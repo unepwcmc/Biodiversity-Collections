225fef63fa683f022d036b20e5987756b794e581
integritas-jozecarlos <i4525001314s>
2016-02-17 18:18:46 -0200

    i18n texts

--- Frontend/app/scripts/i18n/messages.en_GB.properties.js
+++ Frontend/app/scripts/i18n/messages.en_GB.properties.js
@@ -7,7 +7,7 @@ define([],function ( ) {
     'ENGLISH':'English',
     'PORTUGUESE':'Portuguese',
     'SEARCH':'Search',
-    'BRAZIL_BIODIVERSITY_COLLECTION':'Biodiversity Collections',
+    'BRAZIL_BIODIVERSITY_COLLECTION':'Brazilian Biodiversity Collections',
     'NEWS':'News',
     'HOME':'Home',
     'SEARCH_RESULTS':'Search Results',
