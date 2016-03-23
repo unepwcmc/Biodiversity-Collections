74d552f57f46abdf3d7d4f0591233e2fb406b780
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-23 12:28:54 -0300

    Security issues fixed.

--- Backend/src/main/java/com/unep/wcmc/biodiversity/security/GlobalMethodSecurityConfig.java
+++ Backend/src/main/java/com/unep/wcmc/biodiversity/security/GlobalMethodSecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, proxyTargetClass = true)
 public class GlobalMethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
     @Autowired
